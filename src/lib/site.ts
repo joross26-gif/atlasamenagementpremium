cab307e81f88a5b90a0246cfaaf1dd812e6f361c
atlas amenagement <joross26@gmail.com>
2026-02-23 10:35:26 -0500

    Update service area in site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -3,6 +3,6 @@ export const SITE = {
   tagline: "Paysagement & Déneigement • Montréal Est",
   phone: "438-795-4120",
   email: "atlasamenagement514@gmail.com",
-  area: "Montréal Est • Tétreaultville & alentours",
+  area: "Montréal Est • Repentigny ,Charlemagne,Terrebonne,Lachenaie",
   formspreeEnvKey: "NEXT_PUBLIC_FORMSPREE_ENDPOINT",
 };
